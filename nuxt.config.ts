f463149d8d44eb87378a62626abb0c875a24a152
vladz <vladzholon@gmail.com>
2023-04-01 09:15:33 +0300

    working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: ['@vuestic/nuxt', '@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  vuestic: {
+    config: {
+      // Config here
+    },
+  },
 })
